c12299149387214a66d28c14bd2bf7b28ca38f54
CTS <cjswotnsdh@naver.com>
2016-07-07 15:20:47 +0900

    added output bitkeeper

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ public class Hello{
  public static void main(String[] angs){
   System.out.println("Hellow Git");
   System.out.println("what is Git?");
-  }
+  System.out.println("2002,Bitkeeper");
+
   
   
  }
